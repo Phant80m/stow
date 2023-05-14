99c561264d6861e03f76ce9ddf3effa4b21d62aa
phant80m <black.pandazayd@gmail.com>
2023-05-14 12:47:34 +1000

    installer, dependency manager > arch only <

--- stow.sh
+++ stow.sh
@@ -36,6 +36,34 @@ RESET='\033[0m'
 #  CORE VARS
 stow_dir="$HOME/dotfiles"
 
+# UNSET COLORS NNO COL
+if [[ $* == *--no-color* ]] || [[ $* == *-nc* ]]; then
+  # Unset color variables
+  unset BLACK
+  unset RED
+  unset GREEN
+  unset YELLOW
+  unset BLUE
+  unset PURPLE
+  unset CYAN
+  unset WHITE
+  unset BOLD_BLACK
+  unset BOLD_RED
+  unset BOLD_GREEN
+  unset BOLD_YELLOW
+  unset BOLD_BLUE
+  unset BOLD_PURPLE
+  unset BOLD_CYAN
+  unset BOLD_WHITE
+  unset BACKGROUND_BLACK
+  unset BACKGROUND_RED
+  unset BACKGROUND_GREEN
+  unset BACKGROUND_YELLOW
+  unset BACKGROUND_BLUE
+  unset BACKGROUND_PURPLE
+  unset BACKGROUND_CYAN
+  unset BACKGROUND_WHITE
+fi
 function _help() {
   echo -e "${BOLD_GREEN}Usage: $0${RESET} [${BOLD_BLUE}action${RESET}]"
   echo -e ""
@@ -43,16 +71,20 @@ function _help() {
   echo -e "  ${BOLD_BLUE}stow${RESET}: Stow the dotfiles to your home directory."
   echo -e "  ${BOLD_BLUE}unstow${RESET}: Unstow the dotfiles from your home directory."
   echo -e "  ${BOLD_BLUE}clean${RESET}: Remove the backup of your original configuration files."
+  echo -e "  ${BOLD_BLUE}deps${RESET}: Installs the dependencies from a file. ${BOLD_RED}! ONLY WORKS ON ARCH ! $RESET"
+  echo -e "  ${BOLD_BLUE}--no-color | -nc ${RESET}: Disable color"
+  echo -e "  ${BOLD_BLUE}install${RESET}: Installs the script $0 to /usr/bin/stowed"
+  echo -e "  ${BOLD_BLUE}uninstall${RESET}: removes the script $0 from /usr/bin/stowed"
   echo -e ""
   echo -e "${BOLD_BLUE}Description:${RESET}"
   echo -e "  This script is used to stow and unstow dotfiles in your home directory."
-  echo -e "  It will symlink files from a directory named ${BOLD_GREEN}\$HOME/dotfiles${RESET} to their"
+  echo -e "  It will symlink files from a directory named ${BOLD_GREEN}\$HOME/dotfiles/.config${RESET} to their"
   echo -e "  corresponding locations in your home directory."
   echo -e ""
   echo -e "  You can use the ${BOLD_BLUE}stow${RESET} action to create symlinks in your home directory."
   echo -e "  If a file already exists in your home directory, it will be overridden by the symlink."
   echo -e "  Before overriding an existing file, the script will create a backup of the file in the"
-  echo -e "  ${BOLD_GREEN}\$HOME/dotfiles/CONFIG_BACKUP${RESET} directory."
+  echo -e "  ${BOLD_GREEN}\$HOME/.stow/CONFIG_BACKUP${RESET} directory."
   echo -e ""
   echo -e "  You can use the ${BOLD_BLUE}unstow${RESET} action to remove the symlinks from your home directory."
   echo -e "  It will only remove symlinks that were created by the script."
@@ -66,20 +98,32 @@ function _help() {
   echo -e "  To stow the dotfiles, run: ${BOLD_GREEN}$0 stow${RESET}"
   echo -e "  To unstow the dotfiles, run: ${BOLD_GREEN}$0 unstow${RESET}"
   echo -e "  To remove the backup of your original configuration files, run: ${BOLD_GREEN}$0 clean${RESET}"
+  echo -e "  To install the dependencies for the dotfiles run: ${BOLD_GREEN}$0 deps${RESET}"
+
 }
+
+
 if [[ "$1" == "--help" ]]; then
   # help --> 
 _help
 elif [[ "$1" == "stow" ]]; then
   # create a backup just in case
-  backup="${HOME}/.stowed/CONFIG_BACKUP"
-  if [ -d "$backup" ]; then
-    echo -e "${BOLD_YELLOW}[CHECK]: Backup directory already exists. Skipping backup creation.${RESET}"
-  else
+backup="${HOME}/.stowed/CONFIG_BACKUP"
+if [ -d "$backup" ]; then
+  read -p "$(echo -e "${BOLD_YELLOW}[CHECK]: Backup directory already exists. Do you want to create another backup? [y/n]:${RESET}") " choice
+  if [ "$choice" == "y" ]; then
+    backup="${HOME}/.stowed/$(date '+%Y-%m-%d_%H:%M:%S')"
     mkdir -p "$backup"
     cp -r "$HOME/.config/"* "$backup/"
-    echo -e "${BOLD_GREEN}[CHECK]: Backup created in $backup.${RESET}"
+    echo -e "$(echo -e "${BOLD_YELLOW}[CHECK]: Backup created in $backup.${RESET}")"
+  else
+    echo -e "$(echo -e "${BOLD_YELLOW}[CHECK]: Skipping backup creation.${RESET}")"
   fi
+else
+  mkdir -p "$backup"
+  cp -r "$HOME/.config/"* "$backup/"
+  echo -e "$(echo -e "${BOLD_GREEN}[CHECK]: Backup created in $backup.${RESET}")"
+fi
   
   # link dirs
  for dir in "$stow_dir/.config/"*; do
@@ -89,7 +133,7 @@ elif [[ "$1" == "stow" ]]; then
         echo -e "${BOLD_YELLOW}[INFO]: Directory $base_dir exists in $HOME/.config but is empty. Deleting...${RESET}"
         rm -rf "$HOME/.config/$base_dir"
       else
-        echo -e "${BOLD_RED}[CHECK]: Directory $base_dir exists in $HOME/.config and will be overridden.${RESET}"
+        echo -e "${BOLD_RED}[CHECK]: Directory $base_dir exists in $HOME/.config and will be overridden, config is backed up.${RESET}"
         echo -e "${BOLD_YELLOW}[INFO]: Deleting $HOME/.config/$base_dir...${RESET}"
         rm -rf "$HOME/.config/$base_dir"
       fi
@@ -103,13 +147,14 @@ elif [[ "$1" == "stow" ]]; then
     fi
   done
 
+# clean backup
 elif [[ "$1" == "clean" ]]; then
   echo -e "${BOLD_RED}Are you sure you want to delete the config backup?$BOLD_GREEN [ y / n ]$RESET"
   read confirm
 
   if [[ "$confirm" == "y" ]]; then
     # Delete the backup file
-    rm -rf ./CONFIG_BACKUP
+    rm -rf ~/.stowed/*/
     echo -e "${BOLD_GREEN}[INFO]: Backup file $backup has been deleted.${RESET}"
   elif [[ "$confirm" == "n" ]]; then
     # Exit the script
@@ -127,13 +172,12 @@ elif [[ "$1" == "unstow" ]]; then
       echo -e "${BOLD_GREEN}[UNLINKED]: $dir${RESET}"
     fi
   done
+
+
+# install stow as stowed so it doesnt conflict with gnu-stow
 elif [[ "$1" == "install" ]]; then
   sudo cp stow.sh /usr/bin/stowed
   sudo chmod +x /usr/bin/stowed
-
-  # Define color codes
-  GREEN='\033[0;32m'
-  NC='\033[0m'
 
   # Check if fish shell is installed
   if command -v fish >/dev/null 2>&1; then
@@ -144,7 +188,7 @@ elif [[ "$1" == "install" ]]; then
   fi
 
   # Notify user that stow.sh has been installed
-  echo -e "${GREEN}stow.sh has been installed to /usr/bin as stowed. run stowed for help${NC}"
+  echo -e "${BOLD_GREEN}stow.sh has been installed to /usr/bin as stowed. run stowed for help${RESET}"
 
 elif [[ "$1" == "uninstall" ]]; then
   if [ -f /usr/bin/stowed ]; then
@@ -152,6 +196,24 @@ elif [[ "$1" == "uninstall" ]]; then
     echo -e "${BOLD_RED}stowed uninstalled${RESET}"
   else
     echo -e "${BOLD_RED}Error: stowed not found in /usr/bin.${RESET}"
+  fi
+
+# deps instalelr
+elif [[ "$1" == "deps" ]]; then
+  deps_file="$stow_dir/dependencies"
+  if [[ -f "$deps_file" ]]; then
+    if command -v paru >/dev/null 2>&1; then
+      echo -e "${BOLD_YELLOW}[INFO]: using paru for installing dependencies ${RESET}"
+      sudo paru -S --needed $(cat "$deps_file")
+    elif command -v yay >/dev/null 2>&1; then
+      echo -e "${BOLD_YELLOW}[INFO]: using yay for installing dependencies ${RESET}"
+      sudo yay -S --needed $(cat "$deps_file")
+    else
+      echo -e "${BOLD_RED}[WARNING]: paru and yay are not installed, using pacman. AUR dependencies may fail. Please install paru or yay.${RESET}"
+      sudo pacman -S --needed $(cat "$deps_file")
+    fi
+  else
+    echo -e "${BOLD_RED}No dependencies file found in $stow_dir/dependencies \n Create the file $stow_dir/dependencies $RESET"
   fi
 
 else
